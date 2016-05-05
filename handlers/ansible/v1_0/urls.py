185b52ea8ffa0bd2ce53c20136820295cb19151e
mma <majingrzl@163.com>
2016-05-05 16:48:09 +0800

    修改url

--- handlers/ansible/v1_0/urls.py
+++ handlers/ansible/v1_0/urls.py
@@ -7,10 +7,10 @@ from handlers.ansible.v1_0.api import *
 VERSION = 'v1.0'
 
 urls = [
-    url(r'/modules/all/?$', GenModulesHandler),
-    url(r'/modules/$', ExecPlayHandler),
-    url(r'/modules/result/$', ExecPlayResultHandler),
-    url(r'/modules/file/$', ExecPlayBookHandler),
-    url(r'/modules/file/result/$', ExecPBResultHandler),
+    url(r'/modules/all$', GenModulesHandler),
+    url(r'/modules$', ExecPlayHandler),
+    url(r'/modules/result$', ExecPlayResultHandler),
+    url(r'/modules/file$', ExecPlayBookHandler),
+    url(r'/modules/file/result$', ExecPBResultHandler),
 ]
 
