5cbcdabece5e903ad198bec80946658c9ce362aa
mma <majingrzl@163.com>
2016-05-17 11:53:45 +0800

    添加删除数据接口

--- handlers/permission/v1_0/api.py
+++ handlers/permission/v1_0/api.py
@@ -12,7 +12,7 @@ from tornado.concurrent import run_on_executor
 from concurrent.futures import ThreadPoolExecutor
 from common.base import RequestHandler
 from utils.auth import auth
-from resource import get_perm_info, get_all_objects, get_one_object, save_object, update_object
+from resource import get_perm_info, get_all_objects, get_one_object, save_object, update_object, delete_object
 
 logger = logging.getLogger()
 
@@ -28,7 +28,7 @@ class PermObjectsHandler(RequestHandler):
             obj_name = kwargs.get('obj_name')
             obj_id = kwargs.get('obj_id')
             logger.info('obj_id:%s'%obj_id)
-            if obj_id == '':
+            if obj_id == 'all':
                 perm_objs = get_all_objects(obj_name)
             else:
                 perm_objs = get_one_object(obj_name, obj_id)
@@ -95,6 +95,31 @@ class PermObjectsHandler(RequestHandler):
             logger.error(traceback.format_exc())
             self.set_status(500, 'failed')
             self.finish({'messege':'failed'})
+
+    @auth
+    def delete(self, *args, **kwargs):
+        """
+        删除数据
+        """
+        try:
+            obj_name = kwargs.get('obj_name')
+            obj_id = kwargs.get('obj_id')
+            msg = delete_object(obj_name, obj_id)
+            self.set_status(200, 'success')
+            self.finish({'messege': msg})
+        except ValueError:
+            logger.error(traceback.format_exc())
+            self.set_status(400, 'value error')
+            self.finish({'messege':'value error'})
+        except HTTPError as http_error:
+            logger.error(traceback.format_exc())
+            self.set_status(http_error.status_code, http_error.log_message)
+            self.finish({'messege':http_error.log_message})
+        except:
+            logger.error(traceback.format_exc())
+            self.set_status(500, 'failed')
+            self.finish({'messege':'failed'})
+
 
 
 class PermInfoHandler(RequestHandler):
