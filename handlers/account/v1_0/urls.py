46130f8b8840a70ad11ea56442bccff5321a6a2d
mma <majingrzl@163.com>
2016-05-19 14:02:23 +0800

    删除不用的代码

--- handlers/account/v1_0/urls.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-from tornado.web import url
-from handlers.account.v1_0.api import *
-
-VERSION = 'v1.0'
-
-urls = [
-    url(r'/account/(?P<obj_name>.*)/(?P<obj_id>.*)$', UserHandler),
-]
