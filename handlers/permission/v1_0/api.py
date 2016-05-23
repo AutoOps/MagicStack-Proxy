29ccb37f65ea7b64c804c8ec07ccfd4c5d866e6d
mma <majingrzl@163.com>
2016-05-23 10:43:46 +0800

    删除不用的代码

--- handlers/permission/v1_0/api.py
+++ handlers/permission/v1_0/api.py
@@ -123,32 +123,6 @@ class PermObjectsHandler(RequestHandler):
             self.finish({'messege':'failed'})
 
 
-
-class PermInfoHandler(RequestHandler):
-    """
-        获取推送用户所需的信息，asset, user, permrole
-    """
-    @auth
-    def get(self, *agrs, **kwargs):
-        try:
-            role_id = kwargs.get('role_id')
-            perm_info = get_perm_info(role_id)
-            self.set_status(200, 'success')
-            self.finish({'messege': perm_info})
-        except ValueError:
-            logger.error(traceback.format_exc())
-            self.set_status(400, 'value error')
-            self.finish({'messege':'value error'})
-        except HTTPError, http_error:
-            logger.error(traceback.format_exc())
-            self.set_status(http_error.status_code, http_error.log_message)
-            self.finish({'messege':http_error.log_message})
-        except:
-            logger.error(traceback.format_exc())
-            self.set_status(500, 'failed')
-            self.finish({'messege':'failed'})
-
-
 class PushEventHandler(RequestHandler):
     """
         查询用户推送结果
